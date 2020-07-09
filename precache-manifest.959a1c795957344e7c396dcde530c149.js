22569b6e9ac8edf530d30648d71c6a0f53664658
akatsukifood <jean.delaleau@gmail.com>
2020-07-09 10:51:50 +0200

    Updates

---
+++ precache-manifest.959a1c795957344e7c396dcde530c149.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f859edbcaae16c89405b4407badd96df",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3b5c988c182a914737c8",
+    "url": "/monsters-rolodex/static/css/main.f5105599.chunk.css"
+  },
+  {
+    "revision": "4cc6851a78b27cd5950f",
+    "url": "/monsters-rolodex/static/js/2.e8451340.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.e8451340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b5c988c182a914737c8",
+    "url": "/monsters-rolodex/static/js/main.5588a189.chunk.js"
+  },
+  {
+    "revision": "2b67db9d3f05db9a832f",
+    "url": "/monsters-rolodex/static/js/runtime-main.a0203d79.js"
+  }
+]);
